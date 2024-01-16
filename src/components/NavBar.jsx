1df5a8804787ce25984180d61a03a6fc13665fb5
jason.hough1@googlemail.com <jason.hough1@googlemail.com>
2024-01-16 14:32:40 +0000

    avatar and role

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,11 +7,15 @@ import { WelcomeName } from "./WelcomeName";
 import { SignInButton } from "./SignInButton";
 import { SignOutButton } from "./SignOutButton";
 import { Link as RouterLink } from "react-router-dom";
-import { Stack } from "@mui/material";
+import { Avatar, Stack } from "@mui/material";
 import sse_logo from "../logo/sse_logo.png"
 
 import { useIsAuthenticated } from "@azure/msal-react";
+import { deepOrange } from "@mui/material/colors";
 //{isauth ? <WelcomeName /> : null}
+
+
+
 const NavBar = () => {
     const isauth = useIsAuthenticated();
     return (
@@ -19,16 +23,16 @@ const NavBar = () => {
             <AppBar position="static">
                 <Toolbar>
                 <img src={sse_logo} alt="logo" style={{maxHeight: "90px"}} />
-                <Button  color="inherit" >
-                    <b>New Account</b>
+                <Button  color="secondary">
+                    New Account
                 </Button>
-                <Button  color="inherit">
+                <Button  color="secondary">
                     Deactivate Account
                 </Button>
-                <Button  color="inherit">
+                <Button  color="secondary">
                     Adjust Debt
                 </Button>
-                <Button  color="inherit">
+                <Button  color="secondary">
                     Override top-up
                 </Button>
                 
@@ -38,6 +42,7 @@ const NavBar = () => {
                     
                     <Button component={RouterLink} to="/profile" color="inherit">Profile</Button>
                     {isauth ? <SignOutButton /> : <SignInButton />}
+                    {isauth ?<Avatar sx={{ bgcolor: deepOrange[500]}}>B</Avatar> : null}
                 </Toolbar>
             </AppBar>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,26 +2,55 @@ import Typography from "@mui/material/Typography";
 import { AuthenticatedTemplate, UnauthenticatedTemplate , useMsal} from "@azure/msal-react";
 
 import { Button } from "@mui/material";
+import { useState } from "react";
+
+function Counter({initialState}) {
+    const [count, setCount] = useState(initialState);
+    return (
+        <>
+        Count: {count}
+        <button onClick={() => setCount(initialState)}>Reset</button>
+        <button onClick={() => setCount(setCount => setCount -1)}>-</button>
+        <button onClick={() => setCount(setCount => setCount +1)}>+</button>
+
+        </>
+    )
+}
 
 export const Home = () => {
 
     const { instance } = useMsal();
     const activeaccount = instance.getActiveAccount();
-    
+    const [Role, SetRole] = useState(null);
+    var groupmemebership = false;
+    var rolesdefined = true;
+    var role = false;
 
     const benz = () => {
         if (activeaccount.idTokenClaims?.roles === undefined)
         {
+            rolesdefined = false
+            SetRole("No roles defined");
             console.log("No roles defined")
         } else {
             for (let i = 0; i < activeaccount.idTokenClaims.roles.length; i++) {
-                if (activeaccount.idTokenClaims.roles[i] == "Jokers")
+                if (activeaccount.idTokenClaims.roles[i] === "Jokers")
                 {
-                    console.log("Jokers role added")
-                } else {
-                    console.log("Incorrect role added")
-                }
+                    role = true;
+                    SetRole("Correnct role added");
+                    
+                } 
+
             }
+            
+        }
+        if (role === false && rolesdefined === true) {
+           console.log("Incorrect role added");
+           SetRole("Incoorect role added");
+        }
+
+        if (role === true) {
+            console.log("Correct role added");
         }
         
         
@@ -35,10 +64,13 @@ export const Home = () => {
         <AuthenticatedTemplate>
         <Typography variant="h6">You are signed-in. Press the button to see if you have group access.</Typography>
         <Button onClick={benz}>Group Access</Button>
+        <a>{Role}</a>
+
+
         </AuthenticatedTemplate>
 
         <UnauthenticatedTemplate>
-        <Typography variant="h6">Please sign-in to see your profile information.</Typography>
+        <Typography variant="h6">Please sign-in to see your group and profile information.</Typography>
         </UnauthenticatedTemplate>
             
             
